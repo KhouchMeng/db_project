c9b7ca3dff403f61bc4a06d2d40c3b34d382d5ef
KhouchMeng <khouchmeng6@gmail.com>
2024-09-14 12:27:12 +0700

    first commit

---
+++ external.js
@@ -0,0 +1,16 @@
+var id=1; // ការប្រកាសអញ្ញាតិ
+            id=100;
+        var name='Jonh son';
+            name="Kanha";
+        let gender="Male";
+            gender="Female"
+        let age =23;
+        const score=100.00;
+           
+
+        console.log('Value ID :'+id);
+        console.log('Value Name :'+name);
+        console.log('Value Gender :'+gender);
+        console.log('Value AGe :'+age);
+        console.log('Value Score :'+score);
+ 
